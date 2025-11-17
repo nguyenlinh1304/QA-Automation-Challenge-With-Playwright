c286bfa4ba2273a1c912405b2676ad667a233904
thilinh.nguyen <thilinh.nguyen@ayp-group.com>
2025-11-17 23:28:35 +0700

    update cid cd

--- main/tests/e2e/Search/TC-3.e2e.ts
+++ main/tests/e2e/Search/TC-3.e2e.ts
@@ -9,7 +9,7 @@ test.beforeEach(async ({ page }) => {
     await auth.login();
 });
 
-test('[OD-3] Should successfully search user by Username ', async ({
+test('[OD-2] Should successfully search user by Username ', async ({
     page, request
 }) => {
     const commonPage = new CommonPage(page);
